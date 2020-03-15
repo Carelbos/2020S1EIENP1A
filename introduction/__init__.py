c99636f54b3c28d360a558cee781519f6bea53b3
Carelbos <56919622+Carelbos@users.noreply.github.com>
2020-03-15 13:34:21 +0200

    Update __init__.py

--- introduction/__init__.py
+++ introduction/__init__.py
@@ -8,30 +8,30 @@ def exists():
     
 @check50.check(exists)
 def compiles():
-    """cla_lab_4_1_4_1-A.c compiles"""
+    """month compiles"""
     check50.c.compile("month")
 
 @check50.check(compiles)
 def test1():
     """ 1 outputs January"""
-    check50.run("./month").stdin("1").stdout("January").exit(0)
+    check50.run("./month").stdin("1").stdout("[Jj]anuary").exit(0)
 
 @check50.check(compiles)
 def test2():
     """ 6 outputs June"""
-    check50.run("./month").stdin("6").stdout("June").exit(0)
+    check50.run("./month").stdin("6").stdout("[Jj]une").exit(0)
     
 @check50.check(compiles)
 def test3():
     """ 12 outputs December"""
-    check50.run("./month").stdin("12").stdout("December").exit(0)
+    check50.run("./month").stdin("12").stdout("[Dd]ecember").exit(0)
     
 @check50.check(compiles)
 def test4():
     """ -1 outputs Error: no such month in my calendar"""
-    check50.run("./month").stdin("-1").stdout("Error: no such month in my calendar").exit(0)
+    check50.run("./month").stdin("-1").stdout("[Ee]rror: no such month in my calendar").exit(0)
     
 @check50.check(compiles)
 def test5():
     """ 13 outputs Error: no such month in my calendar"""
-    check50.run("./month").stdin("13").stdout("Error: no such month in my calendar").exit(0)
+    check50.run("./month").stdin("13").stdout("[Ee]rror: no such month in my calendar").exit(0)
